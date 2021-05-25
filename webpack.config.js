49d6a968a5f5ec0718fb38ce30e2aedbb8ac46c4
Josiah Leon <sjosiahleon@gmail.com>
2021-05-24 17:34:01 -0700

    Init - webpackConfig/package.json

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+// this will need changes
+
+module.exports = {
+    entry: "./frontend/entry.jsx",
+    output: {
+        path: path.resolve(__dirname),
+        filename: 'bundle.js'
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx', '.*']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/env', '@babel/react']
+                  }
+                }
+            }
+        ]
+    }
+}
