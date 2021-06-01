3347954bf788c8e4ce094e2555e6974cf98c406e
Josiah Leon <sjosiahleon@gmail.com>
2021-06-01 13:54:14 -0700

    React setup complete

---
+++ frontend/entry.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    let root = document.getElementById('root');
+    ReactDOM.render(<h1>Welcome to Vapos!</h1>, root)
+})
