239b8c1a44a1c1f52ea200e309f8ea30f88abe53
Josiah Leon <sjosiahleon@gmail.com>
2021-07-19 16:22:19 -0700

    seeded more games

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,7 +46,7 @@ split = User.create({
     desc: 'What does Terran want with Split?'
 })
 
-# template = user.create({
+# template = User.create({
 #     username: '',
 #     password: '',
 #     email: '',
@@ -60,20 +60,20 @@ Game.delete_all
 
 from_the_depths = Game.create({
     title: 'From The Depths',
-    description: 'voxel wartime vehicle builder',
+    description: 'Over 1000 unique components allow you to build and command voxel vehicles from the deep ocean to outer space- including battleships, planes, submarines and space ships! Forge strategies, allegiances and fleets strong enough to destroy eight deadly factions of the planet and reign supreme.',
     cost: 24.99,
     images_url: 'https://i.ibb.co/nwn1BVw/0-ftd.jpg<SEPA>https://i.ibb.co/m9kj5r0/1-ftd.jpg<SEPA>https://i.ibb.co/s90xJqg/2-ftd.jpg<SEPA>https://i.ibb.co/KKTz6zv/3-ftd.jpg<SEPA>https://i.ibb.co/72SrYLC/4-ftd.jpg<SEPA>https://i.ibb.co/tZJ0mVg/5-ftd.jpg'
     })
 
 supcom_fa = Game.create({
     title: 'Supreme Commander Forged Alliance',
-    description: 'Best RTS ever made',
+    description: 'The last days of man are at hand.. Two years after the Infinite War the once great warring nations now lie in ruins, and humanity’s hope for a brighter future is nothing but a bitter memory.',
     cost: 12.99,
     images_url: 'https://i.ibb.co/FWH8zsc/0-supcomfa.jpg<SEPA>https://i.ibb.co/sJdWVW6/1-supcomfa.jpg<SEPA>https://i.ibb.co/WF66TNm/2-supcomfa.jpg<SEPA>https://i.ibb.co/pXYwRNG/3-supcomfa.jpg<SEPA>https://i.ibb.co/mCRFxk1/4-supcomfa.jpg'
 })
 black_desert = Game.create({
     title: 'Black Desert Online',
-    description: 'Massive MMORPG, countless things to do. plus, pretty graphics',
+    description: 'Black Desert is a sandbox, living-world MMORPG. Experience fast-paced, action-packed combat, hunt monsters and huge bosses, fight with friends in a guild to siege nodes and conquer castles, train your life skills such as fishing, trading, crafting, cooking, and much more!',
     cost: 9.99,
     images_url: 'https://i.ibb.co/Cz0vv8D/0-bdo.jpg<SEPA>https://i.ibb.co/nCGmxSS/1-bdo.jpg<SEPA>https://i.ibb.co/rstsmjM/2-bdo.jpg<SEPA>https://i.ibb.co/xFFczss/3-bdo.jpg<SEPA>https://i.ibb.co/jhRLrx8/4-bdo.jpg'
     
@@ -86,7 +86,7 @@ war_thunder = Game.create({
 })
 x3_farnhams_legacy = Game.create({
     title: 'X3 Farnham\'s Legacy',
-    description: 'The classic X3 game is brought back to life with this remaster of a masterpiece',
+    description: 'X3: Farnham\'s Legacy is a new game in the X3 space game series. TRADE, FIGHT, BUILD, THINK in a living and breathing universe. It incorporates a new storyline and yet more additions to the open, free-form gameplay that forms the core of all X series games.',
     cost: 0,
     images_url: 'https://i.ibb.co/D8hGqq1/0-x3fl.jpg<SEPA>https://i.ibb.co/1GW5fvJ/1-x3fl.jpg<SEPA>https://i.ibb.co/ckNMvSN/2-x3fl.jpg<SEPA>https://i.ibb.co/N73VNVP/3-x3fl.jpg<SEPA>https://i.ibb.co/FK8zpCW/4-x3fl.jpg'
 })
@@ -98,13 +98,61 @@ dead_by_daylight = Game.create({
     images_url: 'https://i.ibb.co/Wtr2Jf8/0-dbd.jpg<SEPA>https://i.ibb.co/8BYTMbn/1-dbd.jpg<SEPA>https://i.ibb.co/VCkYS8Q/2-dbd.jpg<SEPA>https://i.ibb.co/x7Xg3Sz/3-dbd.jpg<SEPA>https://i.ibb.co/37vQHFR/4-dbd.jpg'
 })
 
-
-template = Game.create({
-    title: '',
-    description: '',
-    cost: 0,
-    images_url: ''
+stellaris = Game.create({
+    title: 'Stellaris',
+    description: 'Explore a galaxy full of wonders in this sci-fi grand strategy game from Paradox Development Studios. Interact with diverse alien races, discover strange new worlds with unexpected events and expand the reach of your empire. Each new adventure holds almost limitless possibilities.',
+    cost: 49.99,
+    images_url: 'https://i.ibb.co/M6p8RfL/0-stellaris.jpg<SEPA>https://i.ibb.co/6WmnjNf/1-stellaris.jpg<SEPA>https://i.ibb.co/3SZb6x6/2-stellaris.jpg<SEPA>https://i.ibb.co/1R56m7M/3-stellaris.jpg<SEPA>https://i.ibb.co/pJgLVHp/4-stellaris.jpg'
 })
+
+factorio = Game.create({
+    title: 'Factorio',
+    description: 'Factorio is a game about building and creating automated factories to produce items of increasing complexity, within an infinite 2D world. Use your imagination to design your factory, combine simple elements into ingenious structures, and finally protect it from the creatures who don\'t really like you.',
+    cost: 30.00,
+    images_url: 'https://i.ibb.co/8PDDbhk/0-factorio.jpg<SEPA>https://i.ibb.co/BZB8MZR/1-factorio.jpg<SEPA>https://i.ibb.co/zS1Q7hj/2-factorio.jpg<SEPA>https://i.ibb.co/cg0WnvN/3-factorio.jpg<SEPA>https://i.ibb.co/ZGB9cQn/4-factorio.jpg'
+})
+
+gta = Game.create({
+    title: 'Grand Theft Auto',
+    description: 'Grand Theft Auto V for PC offers players the option to explore the award-winning world of Los Santos and Blaine County in resolutions of up to 4k and beyond, as well as the chance to experience the game running at 60 frames per second.',
+    cost: 29.99,
+    images_url: 'https://i.ibb.co/MZzPSGT/0-gta.jpg<SEPA>https://i.ibb.co/dM8MCPn/1-gta.jpg<SEPA>https://i.ibb.co/wC69KNd/2-gta.jpg<SEPA>https://i.ibb.co/pPZxCg5/3-gta.jpg<SEPA>https://i.ibb.co/hKdDWBF/4-gta.jpg'
+})
+
+warframe = Game.create({
+    title: 'Warframe',
+    description: 'Warframe is a cooperative free-to-play third person online action game set in an evolving sci-fi world.',
+    cost: 0,
+    images_url: 'https://i.ibb.co/M2n2hxW/0-warframe.jpg<SEPA>https://i.ibb.co/JrcR7gD/1-warframe.jpg<SEPA>https://i.ibb.co/9rFh6v8/2-warframe.jpg<SEPA>https://i.ibb.co/Msnv397/3-warframe.jpg<SEPA>https://i.ibb.co/fQgTgbD/4-warframe.jpg'
+})
+
+keeperrl = Game.create({
+    title: 'KeeperRL',
+    description: 'Ambitious dungeon simulator with roguelike and RPG elements. Take the role of an evil wizard and study the methods of black magic. Equip your minions and explore the world, murder innocent villagers and burn their homes. Build your dungeon, lay traps and prepare for an assault of angry heroes.',
+    cost: 19.99,
+    images_url: 'https://i.ibb.co/cwThJgC/0-keeperrl.jpg<SEPA>https://i.ibb.co/n7F0pHk/1-keeperrl.jpg<SEPA>https://i.ibb.co/yBRtts4/2-keeperrl.jpg<SEPA>https://i.ibb.co/PW5WKP8/3-keeperrl.jpg<SEPA>https://i.ibb.co/JkQMccr/4-keeperrl.jpg'
+})
+
+gmod = Game.create({
+    title: 'Garry\'s Mod',
+    description: 'Garry\'s Mod is a physics sandbox. There aren\'t any predefined aims or goals. We give you the tools and leave you to play.',
+    cost: 9.99,
+    images_url: 'https://i.ibb.co/KNZ9MGv/0-gmod.jpg<SEPA>https://i.ibb.co/68vRNVF/1-gmod.jpg<SEPA>https://i.ibb.co/685pCgQ/2-gmod.jpg<SEPA>https://i.ibb.co/gvyy6y0/3-gmod.jpg<SEPA>https://i.ibb.co/fXD0QW0/4-gmod.jpg'
+})
+
+elite_dangerous = Game.create({
+    title: 'Elite Dangerous',
+    description: 'Take control of your own starship in a cutthroat galaxy. Elite Dangerous is the definitive massively multiplayer space epic.',
+    cost: 29.99,
+    images_url: 'https://i.ibb.co/CWkq9Qm/0-elite.jpg<SEPA>https://i.ibb.co/yXH12Gr/1-elite.jpg<SEPA>https://i.ibb.co/dgDwjZz/2-elite.jpg<SEPA>https://i.ibb.co/mtX8R7v/3-elite.jpg<SEPA>https://i.ibb.co/vJqMvgV/4-elite.jpg'
+})
+
+# template = Game.create({
+#     title: '',
+#     description: '',
+#     cost: 0,
+#     images_url: ''
+# })
 
 UsersGame.delete_all
 
